1ecf24fadc6f354fe6641b9912cde36c396b63da
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-27 16:37:13 +0000

    Adding tarfile member sanitization to extractall()

--- serve/serve.py
+++ serve/serve.py
@@ -36,7 +36,26 @@ def download_demo():
     weights_dir = os.path.join(data_dir, sly.fs.get_file_name(weights_tar_url))
     if sly.fs.dir_exists(weights_dir) is False:
         with tarfile.open(weights_path) as archive:
-            archive.extractall(weights_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, weights_dir)
 
     print("Weights and demo image are downloaded")
     return image_path, weights_dir
